98ab6b976210b4df39ef40e370669e89c9909ed4
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-12-13 22:10:55 +0530

    newProgream

--- BubbleSort.java
+++ BubbleSort.java
@@ -19,7 +19,7 @@ class BubbleSort{
 
         public static void printArray(int[] a) {
      		   for (int i = 0; i < a.length; i++) {
-        		    System.out.print(a[i] + " ");
+        		    System.out.print(a[i] + "  ");
       		   }
         	System.out.println();  // Print a newline for readability
         }
