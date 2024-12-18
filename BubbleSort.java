5aebee215700f80d517518d4885e68e84547dec1
suryanshpandey61 <suryansh.knipss061@gmail.com>
2024-12-18 22:45:39 +0530

    new Program added

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,16 +3,16 @@ class BubbleSort{
 	public static void bubbleSort(int[] a){
 		int n=a.length;
 		for(int i=0;i<n-1;i++){
-			int x=0;
+			int y=0;
 			for(int j=0;j<n-1-i;j++){
 				if(a[j]>a[j+1]){
 					int temp=a[j];
 					a[j]=a[j+1];
 					a[j+1]=temp;
-					x++;	
+					y++;
 				}
 			}
-			if(x==0)
+			if(y==0)
 				break;
 		}
 	}
